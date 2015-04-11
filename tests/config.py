86b782c6db9b73cc9fd88286508badad96e1cde5
teitei-tk <teitei.tk@gmail.com>
2015-04-12 01:20:09 +0900

    add : testconfig file

---
+++ tests/config.py
@@ -0,0 +1,12 @@
+PARSE_HTML = """<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset='utf-8'/>
+        <title>TestHTML</title>
+    </head>
+    <body>
+        <div class="container">
+            <p>HTML parse test</p>
+        </div>
+    </body>
+</html>"""
